2a777faa7841f469cd63ebc7d6d238fe14816140
Muhammad Maaz <202200491@stu.uob.edu.bh>
2026-04-12 05:26:42 +0300

    completed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  */
 function changeHeadingText() {
   // TODO: Implement this function
+  const heading = document.getElementById("main-heading");
+  heading.textContent = "DOM Manipulation Challenge";
 }
 
 /**
@@ -10,6 +12,8 @@ function changeHeadingText() {
  */
 function changeBoxColor() {
   // TODO: Implement this function
+  const box = document.getElementById("box-to-modify");
+  box.style.backgroundColor = "lightblue";
 }
 
 /**
@@ -17,6 +21,12 @@ function changeBoxColor() {
  */
 function addNewItem() {
   // TODO: Implement this function
+  const newItem = document.createElement("li");
+  newItem.textContent = "New Item";
+  newItem.classList.add("item"); // keeps styling consistent
+
+  const list = document.getElementById("item-list");
+  list.appendChild(newItem);
 }
 
 /**
@@ -24,6 +34,8 @@ function addNewItem() {
  */
 function highlightParagraph() {
   // TODO: Implement this function
+  const para = document.querySelector(".content-para");
+  para.classList.add("highlight");
 }
 
 /**
@@ -31,18 +43,20 @@ function highlightParagraph() {
  */
 function removeElement() {
   // TODO: Implement this function
+  const element = document.getElementById("to-be-removed");
+  element.remove();
 }
 
 
 // Do not edit the lines below.
 // These lines are for testing purposes.
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = {
-        changeHeadingText,
-        changeBoxColor,
-        addNewItem,
-        highlightParagraph,
-        removeElement
-    };
+  module.exports = {
+    changeHeadingText,
+    changeBoxColor,
+    addNewItem,
+    highlightParagraph,
+    removeElement
+  };
 }
 
